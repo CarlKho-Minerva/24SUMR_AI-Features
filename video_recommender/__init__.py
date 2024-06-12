974a91869a4c6d00fc0514fd761da8ab8f09a0f2
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-06-12 23:34:22 +0900

    chore: Restore .py files

---
+++ video_recommender/__init__.py
@@ -0,0 +1,6 @@
+# video_recommender/__init__.py
+"""
+Video Recommender Package
+"""
+
+__version__ = "1.0.0"
---
+++ video_recommender/demo.py
@@ -0,0 +1,6 @@
+# demo.py
+
+from video_recommender.main import main
+
+if __name__ == "__main__":
+    main()
